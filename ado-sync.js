ede625b0d305188f1dd708b8fb24575883150246
coldflau82 <DavidMauricio.Flautero@thomsonreuters.com>
2026-08-14 13:08:21 -0600

    Update ado-sync.js

--- ado-sync.js
+++ ado-sync.js
@@ -20,6 +20,12 @@ app.get('/favicon.png', (req, res) => {
 
 const { Redis } = require('@upstash/redis');
 
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN
+});
+
+
 // Cliente Redis (lee automáticamente las env vars si se llaman KV_REST_API_URL / KV_REST_API_TOKEN)
 const redis = Redis.fromEnv();
 // Si tus variables tienen otro nombre, usa:
